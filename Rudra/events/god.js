c34003d5d7a06354d4d8514c46d5c349a16c2070
shehzadi309 <kattarhindu8080@gmail.com>
2025-07-15 06:52:19 +0530

    Update god.js

--- Rudra/events/god.js
+++ Rudra/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
 	version: "1.0.0",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "Memer Ladka",
 	description: "Record bot activity notifications!",
     envConfig: {
         enable: true
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100040426712109";
+  var god = "61577803842496";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
